e9aeee6b150806b8f74111cb3fbacf53ad7ad5fe
Quoc <nguyenhungquoc1410@gmail.com>
2023-02-06 22:33:59 +0700

    [ADD] use data from FireStore

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,16 +1,20 @@
-import { cardDetails } from '@/utils/cardDetails'
 import type { NextComponentType, NextPageContext } from 'next'
 import Link from 'next/link'
 
-interface Props {}
+interface Props {
+    data: {
+        name: string
+        description: string
+        longText: string
+        time: string
+    }[]
+}
 
-const Timeline: NextComponentType<NextPageContext, {}, Props> = (
-    props: Props
-) => {
+const Timeline: NextComponentType<NextPageContext, {}, Props> = ({ data }) => {
     return (
         <div className="container mx-auto flex justify-center">
             <ol className="relative border-l border-gray-700">
-                {cardDetails.map((detail, index) => {
+                {data.map((detail, index) => {
                     const { description, time, longText, name } = detail
                     return (
                         <li className="mb-10 ml-6" key={`detail_${index}`}>
@@ -22,17 +26,20 @@ const Timeline: NextComponentType<NextPageContext, {}, Props> = (
                             )}
                             {time && (
                                 <time className="ml-2 mb-2 block text-sm font-normal leading-none text-gray-500">
-                                    {detail.time}
+                                    {time}
                                 </time>
                             )}
                             {longText && (
-                                <p id='long_text' className="ml-2 mb-4 max-w-md overflow-hidden text-ellipsis whitespace-nowrap text-base font-normal text-gray-400">
-                                    {detail.longText}
+                                <p
+                                    id="long_text"
+                                    className="ml-2 mb-4 max-w-md overflow-hidden text-ellipsis whitespace-nowrap text-base font-normal text-gray-400"
+                                >
+                                    {longText}
                                 </p>
                             )}
                             {name && (
                                 <Link
-                                    href={`/date/${detail.name}`}
+                                    href={`/date/${name}`}
                                     className="ml-2 inline-flex items-center rounded-lg border border-gray-600 bg-gray-800 px-4 py-2 text-sm font-medium text-gray-400 hover:bg-gray-700 hover:text-white focus:z-10 focus:text-yellow-700 focus:outline-none focus:ring-4 focus:ring-gray-700"
                                 >
                                     Read More &gt;&gt;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
 import Clock from '@/components/Clock'
 import Timeline from '@/components/Timeline'
+import { db } from '@/utils/firebase'
+import { getDocs, collection, DocumentData } from 'firebase/firestore'
+import { NextComponentType, NextPageContext } from 'next'
 
-export default function Home() {
+interface Props {
+    data: {
+        name: string
+        description: string
+        longText: string
+        time: string
+    }[]
+}
+
+export const getStaticProps = async () => {
+    const querySnapshot = await getDocs(collection(db, 'content'))
+    let data: DocumentData[] = []
+    querySnapshot.forEach((doc) => {
+        data.push({ ...doc.data(), name: doc.id })
+    })
+    return {
+        props: { data: data },
+    }
+}
+
+const Home: NextComponentType<NextPageContext, {}, Props> = ({ data }) => {
     return (
         <>
             <section id="clock" className="bg-cover bg-fixed bg-center">
                 <Clock />
             </section>
             <section className="bg-black p-12">
-                <Timeline />
+                <Timeline data={data} />
             </section>
         </>
     )
 }
+
+export default Home
